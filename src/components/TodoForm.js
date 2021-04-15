5239075f9cdb34defded5461055b14477552c914
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-04-15 16:26:27 -0300

    todo final commit :tada:

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,29 +1,33 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addItem } from '../actions/listActions'
 
 
 function TodoForm(props) {
   const [text, setText] = useState("");
+  const dispatch = useDispatch()
 
   function handleChange(event) {
     let t = event.target.value;
     setText(t);
   }
 
-  function addItem(event) {
+  function addItemEvent(event) {
     event.preventDefault();
 
     if (text) {
       // setItems([...items, text]);
       // estou compartilhando um estado entre componentes
-      props.onAddItem(text);
+      dispatch(addItem(text))
       setText("");
+      props.onHideModal()
     }
   }
 
   return (
     <form>
       <input className="todoInput" onChange={handleChange} type="text" value={text} placeholder="To Do"/>
-      <button className="addTodoButton" onClick={addItem}>Add</button>
+      <button className="addTodoButton" onClick={addItemEvent}>Add</button>
     </form>
   );
 }
